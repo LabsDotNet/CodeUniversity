ac9ac1c8d0482092dbe5eb42ff57a17303a80667
Hugo Rodriguez <harf2310@gmail.com>
2018-12-15 12:26:38 -0500

    Add initial asp files

---
+++ CodeUniversity.Infrastructure/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeUniversity.Infrastructure
+{
+    public class Persona
+    {
+        private string _FirstName;
+
+        private string _LastName;
+
+        public string FullName { get { return $"{_FirstName} {_LastName}"; } }
+
+
+        public void DoSOmething(string value)
+        {
+            var valueFormatted = string.Format("{0} {1}",_FirstName,_LastName);
+        }
+    }
+}
+
